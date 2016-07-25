90de079c752ecd9e24d0894205feec7721701055
David Losada <david@tuxpiper.com>
2016-07-25 21:20:21 +0200

    Work those theme postcards a little bit more

--- app/pheme/pheme-module.js
+++ app/pheme/pheme-module.js
@@ -1,17 +1,43 @@
-angular.module('ushahidi.pheme', [])
+require('angular-linkify');
+
+angular.module('ushahidi.pheme', ['linkify'])
+
 .service('PhemeEventsEndpoint', require('./services/endpoints/events.js'))
 
-.config(require('./pheme-routes.js'))
-
 .config(['$provide', function($provide) {
+	$provide.factory('PhemePostViewService', function() {
+		return {
+			getPostType: function() {
+				return this.postType;
+			},
+			setPostType: function(v) {
+				this.postType = v;
+			}
+		}
+	});
+	//
 	$provide.decorator('postCardDirective', [
 		'$delegate',
 		function($delegate) {
 			var directive = $delegate[0];
-			// TODO: how can we switch the template in function of the type being viewed?
+			// TODO: when we have threads, switch the template or build some ng-if thing
 			// Modify the template of the original post card directive
 			directive.templateUrl = 'templates/pheme/card.html';
+			// Assuming we are dealing with themes, expand that post a little bit
+			directive.controller = [ '$scope', 'PhemePostViewService', function($scope, PhemePostViewService) {
+				$scope.postType = PhemePostViewService.getPostType();
+				if ($scope.postType == 'themes') {
+					// process the contents of the theme post type
+					try {
+						$scope.post.featured_tweet = JSON.parse($scope.post.values['theme-featured-tweet'][0]);
+					} catch (e) {
+						// silent failure
+					}
+				}
+			} ];
 			return $delegate;
 		}
 	]);
-}]);
+}])
+
+.config(require('./pheme-routes.js'));
