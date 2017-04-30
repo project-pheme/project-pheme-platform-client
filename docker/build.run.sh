49e5c36a4f990849bdc4494356c0887ee03cd4dc
David Losada <david@tuxpiper.com>
2017-04-30 22:18:13 +0200

    configure build script with env variables

--- docker/build.run.sh
+++ docker/build.run.sh
@@ -2,18 +2,21 @@
 
 set -e
 
+SRC_PATH=${SRC_PATH:-/vols/src}
+OUT_PATH=${OUT_PATH:-/vols/out}
+
 # Ensure volume with source code is present
 check_vols_src() {
-  if [ ! -d /vols/src ]; then
-    echo "No /vols/src with code"
+  if [ ! -d ${SRC_PATH} ]; then
+    echo "No ${SRC_PATH} with code"
     exit 1
   fi
 }
 
 # Ensure volume where we write the build result is present
 check_vols_out() {
-  if [ ! -d /vols/out ]; then
-    echo "No /vols/out for output!"
+  if [ ! -d ${OUT_PATH} ]; then
+    echo "No ${OUT_PATH} for output!"
     exit 1
   fi
 }
@@ -28,7 +31,7 @@ function sync {
     echo "- node_modules"
     echo "- tmp"
   } > /tmp/rsync_exclude
-  rsync -ar --exclude-from=/tmp/rsync_exclude --delete-during /vols/src/ ./
+  rsync -ar --exclude-from=/tmp/rsync_exclude --delete-during ${SRC_PATH}/ ./
 }
 
 # Build the client
@@ -43,7 +46,7 @@ build() {
 bundle() {
   check_vols_out
   local version=${GITHUB_VERSION:-${CI_BRANCH:-v0.0.0}}
-  gulp release --version-suffix=${version} --dest-dir=/vols/out
+  gulp release --version-suffix=${version} --dest-dir=${OUT_PATH}
 }
 
 watch() {
